f1cbcf75a92f52d793979eada490e068ad7cae26
Xiang Zhang <xz95@uw.edu>
2021-03-27 11:32:47 -0700

    add services dir

---
+++ src/components/details.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+      <div>
+        <h1>Details</h1>
+        <img src="" alt=""/>
+        <p>
+          plot
+        </p>
+        <ul>
+          <li>Emma Stone</li>
+          <li>Mark Hamil</li>
+          <li>Harry Zhang</li>
+        </ul>
+      </div>
+  )
+}
+
+export default Details
---
+++ src/components/search.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+      <div>
+        <h1>Search</h1>
+        <input type="text"/>
+        <button>Search</button>
+        <ul>
+          <li>
+            Result1
+          </li>
+          <li>
+            Result2
+          </li>
+          <li>
+            Result3
+          </li>
+        </ul>
+      </div>
+  )
+}
+
+export default Search
